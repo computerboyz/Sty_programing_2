37f585e5e8d83fea28fb0a4db1ca0fccaae3b3d6
stebbi <stefant17@ru.is>
2019-03-26 13:27:53 +0000

    original commit

---
+++ src/com/ru/usty/scheduling/Policy.java
@@ -0,0 +1,16 @@
+package com.ru.usty.scheduling;
+
+/**
+ * 
+ * DO NOT CHANGE THIS ENUM
+ *
+ */
+
+public enum Policy {
+	RR,
+	FCFS,
+	SPN,
+	SRT,
+	HRRN,
+	FB
+}
---
+++ src/com/ru/usty/scheduling/Scheduler.java
@@ -0,0 +1,133 @@
+package com.ru.usty.scheduling;
+
+import com.ru.usty.scheduling.process.ProcessExecution;
+import java.util.List;
+import java.util.ArrayList;
+
+public class Scheduler {
+
+	ProcessExecution processExecution;
+	Policy policy;
+	int quantum;
+	int currentProc;
+	List<Integer> listproc = new ArrayList<Integer>();
+
+	/**
+	 * Add any objects and variables here (if needed)
+	 */
+
+
+	/**
+	 * DO NOT CHANGE DEFINITION OF OPERATION
+	 */
+	public Scheduler(ProcessExecution processExecution) {
+		this.processExecution = processExecution;
+		Thread RR; 
+		/**
+		 * Add general initialization code here (if needed)
+		 */
+	}
+
+	/**
+	 * DO NOT CHANGE DEFINITION OF OPERATION
+	 */
+	public void startScheduling(Policy policy, int quantum) {
+
+		this.policy = policy;
+		this.quantum = quantum;
+
+		/**
+		 * Add general initialization code here (if needed)
+		 */
+
+		switch(policy) {
+		case FCFS:	//First-come-first-served
+			System.out.println("Starting new scheduling task: First-come-first-served");
+			/**
+			 * Add your policy specific initialization code here (if needed)
+			 */
+			break;
+		case RR:	//Round robin
+			System.out.println("Starting new scheduling task: Round robin, quantum = " + quantum);
+			/**
+			 * Add your policy specific initialization code here (if needed)
+			 */
+			break;
+		case SPN:	//Shortest process next
+			System.out.println("Starting new scheduling task: Shortest process next");
+			/**
+			 * Add your policy specific initialization code here (if needed)
+			 */
+			break;
+		case SRT:	//Shortest remaining time
+			System.out.println("Starting new scheduling task: Shortest remaining time");
+			/**
+			 * Add your policy specific initialization code here (if needed)
+			 */
+			break;
+		case HRRN:	//Highest response ratio next
+			System.out.println("Starting new scheduling task: Highest response ratio next");
+			/**
+			 * Add your policy specific initialization code here (if needed)
+			 */
+			break;
+		case FB:	//Feedback
+			System.out.println("Starting new scheduling task: Feedback, quantum = " + quantum);
+			/**
+			 * Add your policy specific initialization code here (if needed)
+			 */
+			break;
+		}
+
+		/**
+		 * Add general scheduling or initialization code here (if needed)
+		 */
+
+	}
+
+	/**
+	 * DO NOT CHANGE DEFINITION OF OPERATION
+	 */
+	public void processAdded(int processID) {
+		listproc.add(processID);
+//		if(policy == FCFS) {
+		if (listproc.size() == 1) {
+			currentProc = processID;
+			processExecution.switchToProcess(processID);
+		}
+		//System.out.println(processExecution.getProcessInfo(currentProc).totalServiceTime - processExecution.getProcessInfo(currentProc).elapsedExecutionTime);
+		//System.out.println((processExecution.getProcessInfo(processID).totalServiceTime- processExecution.getProcessInfo(processID).elapsedExecutionTime));
+		//}else if (policy == SRT) {
+		if((processExecution.getProcessInfo(processID).totalServiceTime- processExecution.getProcessInfo(processID).elapsedExecutionTime) < (processExecution.getProcessInfo(currentProc).totalServiceTime - processExecution.getProcessInfo(currentProc).elapsedExecutionTime)) {
+			processExecution.switchToProcess(processID);
+			currentProc = processID;
+		}
+		}
+		/**
+		 * Add scheduling code here
+		 */
+
+	//}
+
+	/**
+	 * DO NOT CHANGE DEFINITION OF OPERATION
+	 */
+	public void processFinished(int processID) {
+//		if(policy ==  SRT) {
+		listproc.remove(listproc.indexOf(processID));
+		if(listproc.size() != 0) {
+		currentProc = listproc.get(listproc.size()-1);
+		for(int i = 0; i < listproc.size(); i++) {
+			int id = listproc.get(i);
+			if((processExecution.getProcessInfo(id).totalServiceTime - processExecution.getProcessInfo(id).elapsedExecutionTime) < (processExecution.getProcessInfo(currentProc).totalServiceTime - processExecution.getProcessInfo(currentProc).elapsedExecutionTime)) {	
+					currentProc = id;
+				}}
+		processExecution.switchToProcess(currentProc);
+		}
+			
+//		}
+		/**
+		 * Add scheduling code here
+		 */
+	}
+	}
